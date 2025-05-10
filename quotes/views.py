77b7b690bcea1b15f86b0fdc2d9e89c47638a222
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-05-09 20:14:46 -0600

    Section 04-07 Add dynamic logic in data

--- quotes/views.py
+++ quotes/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+days_of_week = {
+    "monday": "Pienso, luego existo",
+    "tuesday": "La vida es un sueño",
+    "wednesday": "El conocimiento es poder",
+    "thursday": "Sé el cambio que quieres ver",
+    "friday": "Solo sé que no sé nada",
+    "saturday": "Vive como si fuera le último dia",
+    "sunday": "Da un poquito más todos los días"
+}
+
 
 def days_week_with_number(request, day):
     return HttpResponse(day)
 
 
 def days_week(request, day):
-    quote_text = None
-    if day == "monday":
-        quote_text = "Pienso, luego existo"
-    elif day == "tuesday":
-        quote_text = "La vida es un sueño"
-    else:
-        return HttpResponseNotFound("No hay frase para este dia.")
-
-    return HttpResponse(quote_text)
+    try:
+        quote_text = days_of_week[day]
+        return HttpResponse(quote_text)
+    except:
+        return HttpResponseNotFound("Este día no existe.")
