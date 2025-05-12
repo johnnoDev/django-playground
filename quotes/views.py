6c2a99eb5eeff2f6b8d64a0815d535726f812f89
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-05-12 16:17:46 -0600

    Section 04-11, 04-12  Reverse functions and return HTML

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 days_of_week = {
@@ -13,17 +14,29 @@ days_of_week = {
 }
 
 
+def index(request):
+    list_items = ""
+    days = list(days_of_week.keys())  # [monday, tuesday...]
+    for day in days:
+        day_path = reverse("day-quote", args=[day])
+        list_items += f"<li><a href=\"{day_path}\">{day}</a></li>"
+
+    response_html = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_html)
+
+
 def days_week_with_number(request, day):
     days = list(days_of_week.keys())
     if day > len(days):
-        return HttpResponseNotFound("El dia no existe")
+        return HttpResponseNotFound("<h1>El dia no existe</h1>")
     redirect_day = days[day-1]
-    return HttpResponseRedirect(f"/quotes/{redirect_day}")
+    redirect_path = reverse("day-quote", args=[redirect_day])
+    return HttpResponseRedirect(redirect_path)
 
 
 def days_week(request, day):
     try:
         quote_text = days_of_week[day]
         return HttpResponse(quote_text)
-    except:
+    except KeyError:
         return HttpResponseNotFound("Este día no existe.")
