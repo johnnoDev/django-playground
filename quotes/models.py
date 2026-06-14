ef68e5ce5f986961cfcff193fb24b9167c8ae419
johnnoDev <johnny.lopezvera20@gmail.com>
2026-06-13 23:07:51 -0500

    nuevos cambios

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    id_author = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    id_book = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='books'
+    )
+    isbn = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.title
